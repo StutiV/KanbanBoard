ae5431efc16a5964663afbbb6cdcb77f04fd21e5
Yellow_Ledbetter <stuti_v@protonmail.com>
2021-05-26 01:40:22 +0530

    Custom dragging preview

--- src/styles.ts
+++ src/styles.ts
@@ -10,12 +10,14 @@ export const AppContainer = styled.div`
     width: 100%;
 `;
 
-interface DragPreviewContainerProps {
+type DragPreviewContainerProps = {
   isHidden?: boolean
+  isPreview?: boolean
 };
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
-  opacity: ${props => (props.isHidden ? 0.3 : 1)};
+  transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
+  opacity: ${props => (props.isHidden ? 0 : 1)};
 `;
 
 export const ColumnContainer = styled(DragPreviewContainer)`
@@ -84,4 +86,14 @@ export const NewItemInput = styled.input`
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
   width: 100%;
-`
+`;
+
+export const CustomDragLayerContainer = styled.div`
+  height: 100%;
+  left: 0;
+  pointer-events: none;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+`;
--- src/utils/isHidden.ts
+++ src/utils/isHidden.ts
@@ -4,6 +4,7 @@ export const isHidden = (
     draggedItem: DragItem | null,
     itemType: string,
     id: string
+    isPreview?: boolean
 ): boolean => {
     return Boolean(
         draggedItem && draggedItem.type === itemType && draggedItem.id === id
